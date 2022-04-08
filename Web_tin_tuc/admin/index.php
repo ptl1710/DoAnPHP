e0300b7f686a9a3adc0006f36d6e2d1aa21725c8
ptl1710 <85923244+ptl1710@users.noreply.github.com>
2022-04-08 17:17:01 +0700

    first commit

---
+++ Web_tin_tuc/admin/index.php
@@ -0,0 +1,21 @@
+<?php include 'header.php'; ?>
+<?php include 'sidebar.php'; ?>
+<!-- /. NAV SIDE  -->
+<div id="page-wrapper">
+    <div id="page-inner">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>ADMIN </h2>
+            </div>
+        </div>
+
+        <hr />
+
+        Đây là trang Dashboard
+
+    </div>
+
+</div>
+
+</div>
+<?php include 'footer.php'; ?>
---
+++ Web_tin_tuc/index.php
@@ -0,0 +1,16 @@
+<?php include("header.php"); ?>
+
+
+<div class="container">
+
+    <div class="row">
+
+        <?php include("post.php"); ?>
+
+    </div>
+
+
+</div>
+
+
+<?php include("footer.php"); ?>
